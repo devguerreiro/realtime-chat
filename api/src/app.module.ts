596ceee301943d5082862236d1789d365eb7a670
Luis Guerreiro <luiscvlh11@gmail.com>
2025-12-22 20:34:52 -0300

    feat(api): :sparkles: usa redis como caching default

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CacheModule } from '@nestjs/cache-manager';
+import KeyvRedis from '@keyv/redis';
 
 import { ChatModule } from './chat/chat.module';
 import { MSChatModule } from './microservices/chat/chat.module';
 
 @Module({
-  imports: [CacheModule.register({ isGlobal: true }), ChatModule, MSChatModule],
+  imports: [
+    CacheModule.register({
+      useFactory: () => {
+        return {
+          stores: [new KeyvRedis('redis://localhost:6379')],
+        };
+      },
+      isGlobal: true,
+    }),
+    ChatModule,
+    MSChatModule,
+  ],
 })
 export class AppModule {}
